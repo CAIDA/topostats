75690f976adad96880e620caa1638b771baba29d
Young Hyun <youngh@caida.org>
2010-02-26 11:47:14 -0800

    Re-ordered functions in file to be top-down rather than bottom-up.

--- distance.c
+++ distance.c
@@ -31,6 +31,133 @@ unsigned long graph_radius;
 
 /* ====================================================================== */
 
+void load_graph(void);
+void dump_graph(void);
+void dump_links(Word_t i, Word_t li);
+void compute_distance_metrics(void);
+void compute_node_distance_metrics(Word_t i0);
+void compute_average_distance(void);
+
+
+/* ====================================================================== */
+
+void
+load_graph(void)
+{
+  Word_t pi, i, v, l0, li, d;
+  Word_t *pv;
+
+  num_nodes = num_links = 0;
+
+  pi = 0;  /* previous node ID; 0 == no previous node */
+  l0 = 0;  /* starting index for the links of the current node */
+  li = 1;  /* index of current link */
+  d = 0;   /* node degree */
+
+  while (scanf("%lu %lu", &i, &v) > 0) {
+    if (i == 0 || v == 0) {
+      fputs("ERROR: node IDs must be > 0\n", stderr);
+      exit(1);
+    }
+
+    if (i != pi) {
+      if (pi != 0) {
+	JLI(pv, links, l0);  *pv = d; /* save the degree of the previous node */
+	l0 = li++;
+	d = 0;
+      }
+
+      ++num_nodes;
+      pi = i;
+      JLI(pv, nodes, i);  *pv = l0;
+    }
+
+    ++num_links;
+    JLI(pv, links, li);  *pv = v;
+    ++li;
+    ++d;
+  }
+
+  if (num_nodes > 0) {
+    JLI(pv, links, l0);  *pv = d;  /* save the degree of the last node */
+  }
+
+  num_pairs = (unsigned long long)num_nodes * (num_nodes - 1) / 2;
+  num_links /= 2;  /* count undirected links */
+  printf("loaded %lu nodes, %lu undirected links, %llu pairs\n",
+	 num_nodes, num_links, num_pairs);
+}
+
+
+/* ====================================================================== */
+
+void
+dump_graph(void)
+{
+  Word_t i;
+  Word_t *pv;
+
+  i = 0;
+  JLF(pv, nodes, i);
+  while (pv != NULL) {
+    dump_links(i, *pv);
+    JLN(pv, nodes, i);
+  }
+}
+
+
+/* ====================================================================== */
+
+void
+dump_links(Word_t i, Word_t li)
+{
+  Word_t d;
+  Word_t *pv;
+
+  JLG(pv, links, li);
+  d = *pv;
+
+  while (d > 0) {
+    --d;
+    ++li;
+    JLG(pv, links, li);
+    printf("%lu %lu\n", i, *pv);
+  }
+}
+
+
+/* ====================================================================== */
+
+/*
+** eccentricity of node i = max distance from i
+** graph diameter = max eccentricity in a graph
+** graph radius = min eccentricity in a graph
+*/
+void
+compute_distance_metrics(void)
+{
+  Word_t i;
+  Word_t *pv;
+
+  graph_diameter = 0;
+  graph_radius = 0;
+
+  i = 0;
+  JLF(pv, nodes, i);
+  while (pv != NULL) {
+#ifdef DEBUG
+    printf("\n* node %lu:\n", i);
+#endif
+    compute_node_distance_metrics(i);
+    JLN(pv, nodes, i);
+  }
+
+  compute_average_distance();
+}
+
+
+/* ====================================================================== */
+
 void
 compute_node_distance_metrics(Word_t i0)
 {
@@ -139,121 +266,6 @@ compute_average_distance(void)
 
   printf("average distance = %.3f\n", sum / (double)num_pairs);
   printf("std deviation = %.3f\n", sqrt(sd_q / (sd_i - 1)));
-}
-
-
-/*
-** eccentricity of node i = max distance from i
-** graph diameter = max eccentricity in a graph
-** graph radius = min eccentricity in a graph
-*/
-void
-compute_distance_metrics(void)
-{
-  Word_t i;
-  Word_t *pv;
-
-  graph_diameter = 0;
-  graph_radius = 0;
-
-  i = 0;
-  JLF(pv, nodes, i);
-  while (pv != NULL) {
-#ifdef DEBUG
-    printf("\n* node %lu:\n", i);
-#endif
-    compute_node_distance_metrics(i);
-    JLN(pv, nodes, i);
-  }
-
-  compute_average_distance();
-}
-
-
-/* ====================================================================== */
-
-void
-load_graph(void)
-{
-  Word_t pi, i, v, l0, li, d;
-  Word_t *pv;
-
-  num_nodes = num_links = 0;
-
-  pi = 0;  /* previous node ID; 0 == no previous node */
-  l0 = 0;  /* starting index for the links of the current node */
-  li = 1;  /* index of current link */
-  d = 0;   /* node degree */
-
-  while (scanf("%lu %lu", &i, &v) > 0) {
-    if (i == 0 || v == 0) {
-      fputs("ERROR: node IDs must be > 0\n", stderr);
-      exit(1);
-    }
-
-    if (i != pi) {
-      if (pi != 0) {
-	JLI(pv, links, l0);  *pv = d; /* save the degree of the previous node */
-	l0 = li++;
-	d = 0;
-      }
-
-      ++num_nodes;
-      pi = i;
-      JLI(pv, nodes, i);  *pv = l0;
-    }
-
-    ++num_links;
-    JLI(pv, links, li);  *pv = v;
-    ++li;
-    ++d;
-  }
-
-  if (num_nodes > 0) {
-    JLI(pv, links, l0);  *pv = d;  /* save the degree of the last node */
-  }
-
-  num_pairs = (unsigned long long)num_nodes * (num_nodes - 1) / 2;
-  num_links /= 2;  /* count undirected links */
-  printf("loaded %lu nodes, %lu undirected links, %llu pairs\n",
-	 num_nodes, num_links, num_pairs);
-}
-
-
-/* ====================================================================== */
-
-void
-dump_links(Word_t i, Word_t li)
-{
-  Word_t d;
-  Word_t *pv;
-
-  JLG(pv, links, li);
-  d = *pv;
-
-  while (d > 0) {
-    --d;
-    ++li;
-    JLG(pv, links, li);
-    printf("%lu %lu\n", i, *pv);
-  }
-}
-
-
-/* ====================================================================== */
-
-void
-dump_graph(void)
-{
-  Word_t i;
-  Word_t *pv;
-
-  i = 0;
-  JLF(pv, nodes, i);
-  while (pv != NULL) {
-    dump_links(i, *pv);
-    JLN(pv, nodes, i);
-  }
 }
 
 
