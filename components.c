428cabb8a92bb0092672d85ee636007ef9dbf4a7
Young Hyun <youngh@caida.org>
2010-02-26 14:59:02 -0800

    Added components.c.

---
+++ components.c
@@ -0,0 +1,188 @@
+/*
+** Gives a summary of the connected components found in the input graph.
+*/
+
+#include <stdio.h>
+#include <Judy.h>
+
+Pvoid_t nodes = (Pvoid_t)NULL;
+Pvoid_t links = (Pvoid_t)NULL;
+
+Pvoid_t queue = (Pvoid_t)NULL;
+Pvoid_t visited = (Pvoid_t)NULL;  /* Judy1 */
+
+unsigned long num_nodes, num_links;
+
+/* ====================================================================== */
+
+void load_graph(void);
+void dump_graph(void);
+void dump_links(Word_t i, Word_t li);
+void find_connected_components(void);
+void find_connected_component(Word_t i);
+
+/* ====================================================================== */
+
+void
+load_graph(void)
+{
+  Word_t pi, i, v, l0, li, d;
+  Word_t *pv;
+
+  num_nodes = num_links = 0;
+
+  pi = 0;  /* previous node ID; 0 == no previous node */
+  l0 = 0;  /* starting index for the links of the current node */
+  li = 1;  /* index of current link */
+  d = 0;   /* node degree */
+
+  while (scanf("%lu %lu", &i, &v) > 0) {
+    if (i == 0 || v == 0) {
+      fputs("ERROR: node IDs must be > 0\n", stderr);
+      exit(1);
+    }
+
+    if (i != pi) {
+      if (pi != 0) {
+	JLI(pv, links, l0);  *pv = d; /* save the degree of the previous node */
+	l0 = li++;
+	d = 0;
+      }
+
+      ++num_nodes;
+      pi = i;
+      JLI(pv, nodes, i);  *pv = l0;
+    }
+
+    ++num_links;
+    JLI(pv, links, li);  *pv = v;
+    ++li;
+    ++d;
+  }
+
+  if (num_nodes > 0) {
+    JLI(pv, links, l0);  *pv = d;  /* save the degree of the last node */
+  }
+
+  num_links /= 2;  /* count undirected links */
+  printf("loaded %lu nodes, %lu undirected links\n", num_nodes, num_links);
+}
+
+
+/* ====================================================================== */
+
+void
+dump_graph(void)
+{
+  Word_t i;
+  Word_t *pv;
+
+  i = 0;
+  JLF(pv, nodes, i);
+  while (pv != NULL) {
+    dump_links(i, *pv);
+    JLN(pv, nodes, i);
+  }
+}
+
+
+/* ====================================================================== */
+
+void
+dump_links(Word_t i, Word_t li)
+{
+  Word_t d;
+  Word_t *pv;
+
+  JLG(pv, links, li);
+  d = *pv;
+
+  while (d > 0) {
+    --d;
+    ++li;
+    JLG(pv, links, li);
+    printf("%lu %lu\n", i, *pv);
+  }
+}
+
+
+/* ====================================================================== */
+
+void
+find_connected_components(void)
+{
+  Word_t i;
+  Word_t *pv;
+  int Rc_int;
+  unsigned long num_components=0;
+
+  i = 0;
+  JLF(pv, nodes, i);
+  while (pv != NULL) {
+    J1T(Rc_int, visited, i);
+    if (!Rc_int) {
+      find_connected_component(i);
+      ++num_components;
+    }
+
+    JLN(pv, nodes, i);
+  }
+
+  printf("%lu components\n", num_components);
+}
+
+
+/* ====================================================================== */
+
+void
+find_connected_component(Word_t i0)
+{
+  Word_t qhead, qtail, i, i2, li, deg;
+  Word_t *pv;
+  int Rc_int;
+  unsigned long num_cc_nodes=0, num_cc_links=0;
+
+  qhead = qtail = 0;
+  JLI(pv, queue, qtail);  *pv = i0;  ++qtail;
+
+  J1S(Rc_int, visited, i0);
+  while (qhead != qtail) {
+    ++num_cc_nodes;
+    JLG(pv, queue, qhead);  i = *pv;
+    JLD(Rc_int, queue, qhead);
+    ++qhead;
+
+    JLG(pv, nodes, i);  li = *pv;
+    JLG(pv, links, li);  deg = *pv;
+    num_cc_links += deg;
+
+    while (deg > 0) {
+      --deg;
+      ++li;
+      JLG(pv, links, li);  i2 = *pv;
+      J1T(Rc_int, visited, i2);
+      if (!Rc_int) {
+	J1S(Rc_int, visited, i2);
+	JLI(pv, queue, qtail);  *pv = i2;  ++qtail;
+      }
+    }
+  }
+
+  num_cc_links /= 2;  /* undirected links */
+  printf("component at node %lu: %lu nodes, %lu undirected links\n",
+	 i0, num_cc_nodes, num_cc_links);
+}
+
+
+/* ====================================================================== */
+
+int
+main(int argc, char *argv[])
+{
+  load_graph();
+#ifdef DEBUG
+  dump_graph();
+#endif
+  find_connected_components();
+  return 0;
+}
