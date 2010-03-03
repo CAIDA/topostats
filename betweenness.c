6f0f1d857c3c9699375eb09be2803516d39a5afc
Young Hyun <youngh@caida.org>
2010-03-03 14:38:45 -0800

    Implemented edge betweenness.

--- betweenness.c
+++ betweenness.c
@@ -29,10 +29,54 @@ Pvoid_t distances = (Pvoid_t)NULL;
 */
 Pvoid_t node_index = (Pvoid_t)NULL;
 double *delta = NULL;       /* delta[t]: dependency of s on t */
-double *node_centrality = NULL;  /* C_B[v]: betweenness centrality for node v */
+double *node_centrality = NULL; /* C_B[v]: betweenness centrality for node v */
+
+/*
+** C_B[(v,w)]: betweenness centrality for edge (v, w)
+**
+** Warning: We represent undirected links with a pair of symmetric directed
+**          links and the link betweenness is calculated per *directed*
+**          link.  For summary statistics like mean, min, and max, this
+**          doesn't matter, but it might matter for some things, so you
+**          (the programmer who is thinking of implementing some new
+**          analysis) should keep this in mind.  Specifically, it may
+**          mean you need to limit yourself to the link betweenness value
+**          for just one of the two symmetric directed links (e.g., only
+**          use the value for the direction going from a lower-numbered
+**          node to a higher-numbered node).
+**
+** Unlike with nodes, there is no need to map a sparse link ID to an array
+** index because link IDs aren't present in the input graph and therefore
+** aren't subject to the whims of the user.
+**
+** To index into {edge_centrality}, we need a link ID.  Link IDs are only
+** implicit in our graph representation; namely, each directed link is
+** stored in {links}, and the index of a link in {links} is its ID for the
+** purposes of indexing into {edge_centrality}.  The {links} array also
+** contains some metadata (specifically, the degree of each node), so our
+** use of this implicit link ID will mean {edge_centrality} needs to be
+** larger than it absolutely needs to be.  However, this overhead will be
+** less than using a separate link ID map.  In the worst case of every node
+** having a degree of 1, {edge_centrality} will be twice as large as it
+** needs to be, but the overhead decreases quickly as node degree increases
+** (for example, if all nodes have degree 2, then {edge_centrality} will
+** only be 1.3x as large as it needs to be).  So in practice, the overhead
+** is not an issue.
+*/
+double *edge_centrality = NULL;
 
 unsigned long num_nodes, num_links;
 unsigned long long num_pairs;  /* C(n, 2) pairs of nodes */
+
+/*
+** The number of elements in the {links} array.
+**
+** This is a fairly low-level bit of information and closely tied to the
+** exact details of the graph representation in {nodes} and {links}, but we
+** need this information to determine what size {edge_centrality} should
+** have and for iterating over {edge_centrality}.
+*/
+unsigned long links_array_size;
 
 /* ====================================================================== */
 
@@ -44,9 +88,10 @@ void compute_node_brandes_betweenness_centrality(Word_t s);
 void compute_node_dependency(Word_t s, Pvoid_t L, Word_t ltail, Pvoid_t P,
 			     Pvoid_t sigma);
 void normalize_centrality(void);
-void compute_centrality_statistics(void);
+void compute_centrality_statistics(double *centrality, unsigned long len,
+				   const char *name);
 void free_predecessor_array(Pvoid_t P);
-void zero_array(double *x);
+void fill_array(double *x, unsigned long len, double value);
 
 /* ====================================================================== */
 
@@ -91,6 +136,8 @@ load_graph(void)
   if (num_nodes > 0) {
     JLI(pv, links, l0);  *pv = d;  /* save the degree of the last node */
   }
+
+  links_array_size = (unsigned long)li;
 
   num_pairs = (unsigned long long)num_nodes * (num_nodes - 1) / 2;
   num_links /= 2;  /* count undirected links */
@@ -153,7 +200,8 @@ compute_brandes_betweenness_centrality(void)
   }
 
   normalize_centrality();
-  compute_centrality_statistics();
+  compute_centrality_statistics(node_centrality, num_nodes, "node");
+  compute_centrality_statistics(edge_centrality, links_array_size, "edge");
 }
 
 
@@ -175,6 +223,16 @@ compute_brandes_betweenness_centrality(void)
 **
 **   P[w] -- A list of the predecessors of node w in the shortest
 **        paths from s to w.  Each P[w] is a Judy array.
+**
+**        We need to know the predecessor nodes for computing the _node_
+**        betweenness, and we need to know the predecessor links for
+**        computing the _edge_ betweenness.  For this reason, the P[w]
+**        subarray contains pairs of values: (link ID, node ID).  For
+**        example, if w has the incoming shortest-path links (x, w), (y,
+**        w), and (z, w), with link IDs 1, 2, and 3, then P[w] has the
+**        values [1, x, 2, y, 3, z].  We could just store [1, 2, 3] and
+**        obtain the values x, y, z by indexing into {links}, but storing
+**        the node IDs directly in P[w] improves the locality of reference.
 **
 **   sigma[t] -- The number of shortest paths from s to t.
 **        sigma[s] is 1 by convention.
@@ -249,10 +307,12 @@ compute_node_brandes_betweenness_centrality(Word_t s)
 	JLG(pv, sigma, w);  sigma_w = (pv ? *pv : 0);
 	JLI(pv, sigma, w);  *pv = sigma_w + sigma_v;
 
-	/* append v to P[w] */
+	/* Append v to P[w].  We actually append the pair (link ID, v);
+           see the comments above this function for details. */
 	JLG(pv, P, w);  P_entry = (pv ? (Pvoid_t)*pv : (Pvoid_t)NULL);
 	JLC(Rc_word, P_entry, 0, -1);  /* Rc_word == 0 if P_entry == NULL */
-	JLI(pv, P_entry, Rc_word);  *pv = v;  /* array is created if needed */
+	JLI(pv, P_entry, Rc_word);  *pv = li; /* array is created if needed */
+	JLI(pv, P_entry, Rc_word + 1);  *pv = v;
 	JLI(pv, P, w);  *pv = (Word_t)P_entry;
       }
     }
@@ -278,9 +338,10 @@ compute_node_dependency(Word_t s, Pvoid_t L, Word_t ltail, Pvoid_t P,
 			Pvoid_t sigma)
 {
   Pvoid_t P_entry = (Pvoid_t)NULL;
-  Word_t *pv, j, v, w, vi, wi, sigma_v, sigma_w;
+  Word_t *pv, j, li, v, w, vi, wi, sigma_v, sigma_w;
+  double factor;
 
-  zero_array(delta);
+  fill_array(delta, num_nodes, 0.0);
 
   while (ltail-- > 0) {
     JLG(pv, L, ltail);  w = *pv;
@@ -294,15 +355,26 @@ compute_node_dependency(Word_t s, Pvoid_t L, Word_t ltail, Pvoid_t P,
     JLG(pv, P, w);
     if (pv) {
       P_entry = (Pvoid_t)*pv;
-      for (j = 0; ; j++) {
+      for (j = 0; ; j += 2) {
 	JLG(pv, P_entry, j);
-	if (!pv) break;
+	if (!pv) break;  /* end of P_entry array */
+	li = *pv;  /* link ID for incoming link */
 
-	v = *pv;
+	JLG(pv, P_entry, j + 1);
+	v = *pv;   /* predecessor node */
+
 	JLG(pv, sigma, v);  sigma_v = *pv;
 	JLG(pv, node_index, v);  vi = *pv;
 
-	delta[vi] += (sigma_v / (double)sigma_w) * (1.0 + delta[wi]);
+	factor = (sigma_v / (double)sigma_w) * (1.0 + delta[wi]);
+	delta[vi] += factor;
+
+	if (edge_centrality[li] < 0.0) {
+	  edge_centrality[li] = factor;
+	}
+	else {
+	  edge_centrality[li] += factor;
+	}
 
 #ifdef DEBUG
 	printf("     P[w]: v = %lu, sigma_v = %lu; vi = %lu; d[vi] = %.5f\n",
@@ -324,8 +396,11 @@ compute_node_dependency(Word_t s, Pvoid_t L, Word_t ltail, Pvoid_t P,
 /* ====================================================================== */
 
 /*
-** Note: Because links are undirected, we need to divide the computed
-**       absolute node centrality values by 2.
+** Normalizes the absolute node and edge centrality values.
+**
+** Note: Because we represent undirected link with a pair of symmetric
+**       directed links, we need to divide the computed absolute node
+**       centrality values by 2 in addition to normalizing by n(n - 1).
 */
 void normalize_centrality(void)
 {
@@ -336,14 +411,25 @@ void normalize_centrality(void)
   while (x < end) {
     *x++ /= d;
   }
+
+  x = edge_centrality;
+  end = x + links_array_size;
+  d = num_nodes * (num_nodes - 1);
+
+  while (x < end) {
+    if (*x >= 0.0) {  /* edge centrality has -1.0 at non-link entries */
+      *x /= d;
+    }
+    ++x;
+  }
 }
 
 
 /* ====================================================================== */
 
 /*
-** Computes the average, std dev, min, and max of the node betweenness
-** centrality.
+** Computes the average, std dev, min, and max of the node and edge
+** betweenness centrality.
 **
 ** The sample standard deviation code is efficient and numerically stable.
 ** However, we compute the average betweenness separately rather than
@@ -352,32 +438,36 @@ void normalize_centrality(void)
 ** slightly less accurate due to round-off errors.
 */
 void
-compute_centrality_statistics(void)
+compute_centrality_statistics(double *centrality, unsigned long len,
+			      const char *name)
 {
-  unsigned long i, sd_i=0;
-  double x, sum, sd_mean, sd_q, min_node_betweenness, max_node_betweenness;
+  unsigned long i, num_values=0, sd_i=0;
+  double x, sum, sd_mean, sd_q, min_betweenness, max_betweenness;
 
 #ifdef DEBUG
-  printf("\n* node centrality distribution:\n");
+  printf("\n* %s centrality distribution:\n", name);
 #endif
 
   sum = sd_mean = sd_q = 0.0;
-  min_node_betweenness = -1.0;
-  max_node_betweenness = 0.0;
+  min_betweenness = -1.0;
+  max_betweenness = 0.0;
 
-  for (i = 0; i < num_nodes; i++) {
-    x = node_centrality[i];
+  for (i = 0; i < len; i++) {
+    x = centrality[i];
+    if (x < 0.0) continue; /* edge centrality has -1.0 at non-link entries */
+
+    num_values += 1;
     sum += x;
 
 #ifdef DEBUG
     printf("  %.5f\n", x);
 #endif
-    if (x > max_node_betweenness) {
-      max_node_betweenness = x;
+    if (x > max_betweenness) {
+      max_betweenness = x;
     }
 
-    if (x < min_node_betweenness || min_node_betweenness < 0) {
-      min_node_betweenness = x;
+    if (x < min_betweenness || min_betweenness < 0) {
+      min_betweenness = x;
     }
 
     /* std dev calculation */
@@ -386,10 +476,10 @@ compute_centrality_statistics(void)
     sd_mean += (x - sd_mean) / sd_i;
   }
 
-  printf("average node betweenness = %.4e\n", sum / num_nodes);
-  printf("std dev of node betweenness = %.4e\n", sqrt(sd_q / (sd_i - 1)));
-  printf("min node betweenness = %.4e\n", min_node_betweenness);
-  printf("max node betweenness = %.4e\n", max_node_betweenness);
+  printf("average %s betweenness = %.4e\n", name, sum / num_values);
+  printf("std dev of %s betweenness = %.4e\n", name, sqrt(sd_q / (sd_i - 1)));
+  printf("min %s betweenness = %.4e\n", name, min_betweenness);
+  printf("max %s betweenness = %.4e\n", name, max_betweenness);
 }
 
 
@@ -415,12 +505,12 @@ free_predecessor_array(Pvoid_t P)
 
 /* ====================================================================== */
 
-void zero_array(double *x)
+void fill_array(double *x, unsigned long len, double value)
 {
-  double *end = x + num_nodes;
+  double *end = x + len;
 
   while (x < end) {
-    *x++ = 0.0;
+    *x++ = value;
   }
 }
 
@@ -438,7 +528,10 @@ main(int argc, char *argv[])
   /* Allocate {delta} once globally to avoid allocating for each source node. */
   delta = (double *)malloc(num_nodes * sizeof(double));
   node_centrality = (double *)malloc(num_nodes * sizeof(double));
-  zero_array(node_centrality);
+  fill_array(node_centrality, num_nodes, 0.0);
+
+  edge_centrality = (double *)malloc(links_array_size * sizeof(double));
+  fill_array(edge_centrality, links_array_size, -1);
 
   compute_brandes_betweenness_centrality();
   return 0;
