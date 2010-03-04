f107bea8f0f9d4f44cf75f1e911ac7c7d55b4687
Young Hyun <youngh@caida.org>
2010-03-03 16:41:26 -0800

    Added GPLv3+ licensing text.

--- betweenness.c
+++ betweenness.c
@@ -8,6 +8,22 @@
 ** See Brandes, "On variants of shortest-path betweenness
 ** centrality and their generic computation," Social Networks, Nov 2007
 ** for the slight changes needed to compute edge betweenness.
+**
+** ---------------------------------------------------------------------
+** Copyright (C) 2010 The Regents of the University of California.
+**
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <stdio.h>
